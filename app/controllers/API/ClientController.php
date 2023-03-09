400df68f09847ee13704dde625e8be1239141d84
Programador Cego <programadorcego@gmail.com>
2023-03-09 08:25:23 -0300

    Aula 23: Criando mais alguns endpoints

--- app/controllers/API/ClientController.php
+++ app/controllers/API/ClientController.php
@@ -15,8 +15,9 @@ class ClientController extends Controller
 		try
 		{
 			$response['status'] = 'success';
-			$response['message'] = 'API running OK!s';
+			$response['message'] = 'API running OK!';
 			$clients = (new Client())->all();
+			$response['total'] = count($clients);
 			$response['clients'] = $clients;
 		} catch(Exception $e)
 		{
@@ -34,9 +35,99 @@ class ClientController extends Controller
 		try
 		{
 			$response['status'] = 'success';
-			$response['message'] = 'API running OK!s';
+			$response['message'] = 'API running OK!';
 			$client = (new Client())->find($id);
-			$response['client'] = $client;
+			$response['client'] = $client ? $client : [];
+		} catch(Exception $e)
+		{
+			$response['status'] = 'error';
+			$response['message'] = 'Error! ' . $e->getMessage();
+		}
+		
+		$response['time_response'] = time();
+		
+		return (new Response(200, $response, 'application/json'))->sendResponse();
+	}
+	
+	public function get_clients_by_name(Request $request)
+	{
+		$queryParams = $request->getQueryParams();
+		
+		$name = $queryParams['name'] ?? "";
+		$name = str_replace(' ', '%', $name);
+		
+		try
+		{
+			$response['status'] = 'success';
+			$response['message'] = 'API running OK!';
+			$clients = (new Client())->read(where: "nome LIKE '%{$name}%'")->fetchAll();
+			$response['total'] = count($clients);
+			$response['clients'] = $response['total'] > 0 ? $clients : [];
+		} catch(Exception $e)
+		{
+			$response['status'] = 'error';
+			$response['message'] = 'Error! ' . $e->getMessage();
+		}
+		
+		$response['time_response'] = time();
+		
+		return (new Response(200, $response, 'application/json'))->sendResponse();
+	}
+	
+	public function get_clients_by_email(Request $request)
+	{
+		$queryParams = $request->getQueryParams();
+		
+		$email = $queryParams['email'] ?? "";
+		$email = str_replace(' ', '%', $email);
+		
+		try
+		{
+			$response['status'] = 'success';
+			$response['message'] = 'API running OK!';
+			$clients = (new Client())->read(where: "email LIKE '%{$email}%'")->fetchAll();
+			$response['total'] = count($clients);
+			$response['clients'] = $response['total'] > 0 ? $clients : [];
+		} catch(Exception $e)
+		{
+			$response['status'] = 'error';
+			$response['message'] = 'Error! ' . $e->getMessage();
+		}
+		
+		$response['time_response'] = time();
+		
+		return (new Response(200, $response, 'application/json'))->sendResponse();
+	}
+	
+	public function get_clients_by_male()
+	{
+		try
+		{
+			$response['status'] = 'success';
+			$response['message'] = 'API running OK!';
+			$clients = (new Client())->read(where: "sexo = 'm'")->fetchAll();
+			$response['total'] = count($clients);
+			$response['clients'] = $response['total'] > 0 ? $clients : [];
+		} catch(Exception $e)
+		{
+			$response['status'] = 'error';
+			$response['message'] = 'Error! ' . $e->getMessage();
+		}
+		
+		$response['time_response'] = time();
+		
+		return (new Response(200, $response, 'application/json'))->sendResponse();
+	}
+	
+	public function get_clients_by_female()
+	{
+		try
+		{
+			$response['status'] = 'success';
+			$response['message'] = 'API running OK!';
+			$clients = (new Client())->read(where: "sexo = 'f'")->fetchAll();
+			$response['total'] = count($clients);
+			$response['clients'] = $response['total'] > 0 ? $clients : [];
 		} catch(Exception $e)
 		{
 			$response['status'] = 'error';
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
 <?php
-$route->get('api/clients', '\\App\\Controllers\\API\\ClientController@get_all_clients');
-$route->get('api/client/{id}', '\\App\\Controllers\\API\\ClientController@get_client');
+$route->get('api/get_all_clients', '\\App\\Controllers\\API\\ClientController@get_all_clients');
+$route->get('api/get_client/{id}', '\\App\\Controllers\\API\\ClientController@get_client');
+$route->get('api/get_clients_by_name', '\\App\\Controllers\\API\\ClientController@get_clients_by_name');
+$route->get('api/get_clients_by_email', '\\App\\Controllers\\API\\ClientController@get_clients_by_email');
+$route->get('api/get_clients_by_male', '\\App\\Controllers\\API\\ClientController@get_clients_by_male');
+$route->get('api/get_clients_by_female', '\\App\\Controllers\\API\\ClientController@get_clients_by_female');
